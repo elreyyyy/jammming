90f9624df1e6e8d59908fc6792fef82a2a00808d
Robin Khan <robnhoohd@gmail.com>
2024-03-24 03:46:11 -0400

    save current status 2

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React dude!
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+        <div>
+        <h1>
+          Ja<span className={styles.highlight}>mmm</span>ing
+        </h1>
+        <div className={styles.App}>
+          {/* <!-- Add a SearchBar component --> */}
+          
+          <div className={styles['App-playlist']}>
+            {/* <!-- Add a SearchResults component --> */}
+            {/* <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
+        );
 }
 
 export default App;
